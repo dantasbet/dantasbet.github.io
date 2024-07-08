c84f81f4838e86c71cab40105ade7ba18ced4e3e
dantasbet <dantasdabet@gmail.com>
2024-07-08 00:22:57 -0300

    Update script.js

--- double/script.js
+++ double/script.js
@@ -7,14 +7,11 @@ function escolherPar() {
         }
 
         function jogar(escolhaUsuario) {
-            // Gerar número aleatório de 1 a 14 para o computador
             var numeroComputador = Math.floor(Math.random() * 14) + 1;
-
-            // Mostrar escolhas e resultado
+                    
             var mensagem = `Você escolheu ${escolhaUsuario}.<br>`;
             mensagem += `O computador escolheu ${numeroComputador}.<br><br>`;
 
-            // Determinar se o usuário ganhou ou perdeu
             if ((escolhaUsuario === "par" && numeroComputador % 2 === 0) ||
                 (escolhaUsuario === "ímpar" && numeroComputador % 2 !== 0)) {
                 mensagem += "Você ganhou!";
